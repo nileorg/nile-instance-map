8aae7f16b9f211e4338634e78ed49c1eb2310162
huhn <sehe89@gmail.com>
2019-05-15 23:14:32 +0200

    add map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Nile Instance Map</h1>
+    <p>This map shows all Nile instances all over the world.</p>
+    <p>Current instances: 0</p>
+    <Map/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 1080px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="map">
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
+      <l-tile-layer :url="url" :attribution="attribution" />
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from "vue2-leaflet";
+export default {
+  name: "Map",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip
+  },
+  data() {
+    return {
+      zoom: 2,
+      center: latLng(47.41322, -1.219482),
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(47.41322, -1.219482),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 5,
+      currentCenter: latLng(47.41322, -1.219482),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      }
+    };
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.map {
+  width: 100%;
+  height: 580px;
+}
+</style>
